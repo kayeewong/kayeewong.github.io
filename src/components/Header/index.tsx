82cc72fe3c6f89e4277871e21c9bd406ad45b794
KayeeWong <jiayihuang0211@gmail.com>
2024-06-06 18:07:13 +0800

    feat: header section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,23 @@
+import css from './index.module.scss'
+import { BiLogoGmail } from "react-icons/bi"
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={`paddings ${css.wrapper}`}>
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={css.name}>
+          Kayee
+        </div>
+        <ul className={`flexCenter ${css.menu}`}>
+          <li><a href="">Experience</a></li>
+          <li><a href="">About</a></li>
+          <li className={`flexCenter ${css.gmail}`}>
+            {/* TODO global vailable */}
+            <p>xxx@gmail.com</p>
+            <BiLogoGmail />
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
