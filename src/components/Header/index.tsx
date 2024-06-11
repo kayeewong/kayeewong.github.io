0fe5d5a16108d932e8fbe8aaca93bc67a2ba161c
KayeeWong <jiayihuang0211@gmail.com>
2024-06-11 17:56:20 +0800

    feat: header motion part

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,40 @@
 import css from './index.module.scss'
-import { BiLogoGmail } from "react-icons/bi"
+import { BiLogoGmail, BiMenuAltRight } from "react-icons/bi"
+import { motion } from 'framer-motion'
+import { getMenuStyles, headerVariants } from '../../utils/motion'
+import { useState } from 'react'
+import useHeaderShadow from '../../hooks/useHeaderShadow'
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false)
+  const headerShadow = useHeaderShadow()
   return (
-    <div className={`paddings ${css.wrapper}`}>
+    <motion.div
+      initial="hidden"
+      whileInView="show"
+      variants={headerVariants}
+      className={`paddings ${css.wrapper}`}
+      style={{ boxShadow: headerShadow }}
+    >
       <div className={`flexCenter innerWidth ${css.container}`}>
         <div className={css.name}>
           Kayee
         </div>
-        <ul className={`flexCenter ${css.menu}`}>
+        <ul
+          style={getMenuStyles(menuOpened)}
+          className={`flexCenter ${css.menu}`}>
           <li><a href="">Experience</a></li>
           <li><a href="">About</a></li>
           <li className={`flexCenter ${css.gmail}`}>
             {/* TODO global vailable */}
-            <p>xxx@gmail.com</p>
+            <p>jiayihuang0211@gmail.com</p>
             <BiLogoGmail />
           </li>
         </ul>
+        <div className={css.menuIcon} onClick={() => setMenuOpened(prev => !prev)}>
+          <BiMenuAltRight size={30} />
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
---
+++ src/utils/motion.ts
@@ -0,0 +1,26 @@
+export const headerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      delay: 1,
+    },
+  },
+}
+
+export const getMenuStyles = (menuOpened: boolean) => {
+  if (document.documentElement.clientWidth <= 640) {
+    return { right: menuOpened ? 0 : "-100%" }
+  }
+}
